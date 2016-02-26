57fbd22ee12d529b6dcc71c067575a69ae0f74e4
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2016-02-26 07:53:31 +0000

    Started to think about charts

--- app/js/controllers/calculation.controller.js
+++ app/js/controllers/calculation.controller.js
@@ -12,7 +12,8 @@
                 addMortgage: addMortgage,
                 calculate: calculate,
                 deleteComparison: deleteComparison,
-                save: saveModel
+                save: saveModel,
+                showChart: showChart
             },
             data: {
                 statements: []
@@ -54,6 +55,7 @@
                 let statement = CalculationService.getYearlyPayments(balance, comparison, monthlyPayment);
                 $scope.data.statements.push(statement);
             }
+            return true;
         }
 
         function canAddMortgage(comparison) {
@@ -106,6 +108,40 @@
 
         function saveModel() {
             return StorageService.set('model', $scope.model);
+        }
+
+        function showChart() {
+            var ctx = document.getElementById("myChart").getContext("2d");
+
+            var data = {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(220,220,220,0.2)",
+                        strokeColor: "rgba(220,220,220,1)",
+                        pointColor: "rgba(220,220,220,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    {
+                        label: "My Second dataset",
+                        fillColor: "rgba(151,187,205,0.2)",
+                        strokeColor: "rgba(151,187,205,1)",
+                        pointColor: "rgba(151,187,205,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(151,187,205,1)",
+                        data: [28, 48, 40, 19, 86, 27, 90]
+                    }
+                ]
+            };
+
+            var myLineChart = new Chart(ctx).Line(data, {
+                bezierCurve: false
+            });
         }
     }
 })();
