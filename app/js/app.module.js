d72c21cba427459d13f70af3025fdbd1470c7f8c
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2016-03-02 08:30:31 +0000

    Decided on angular-chart.js directive.

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -3,10 +3,6 @@
 
     let requires = [];
     requires.push('as.sortable');
-    //requires.push('angles');          // angles.js
     requires.push('chart.js');          // angular-chart.js
-    //requires.push('chartjs');           // angular-chartjs.js
-    //requires.push('chartjs-directive'); // chartjs-directive.js
-
     angular.module('app', requires);
 })();
